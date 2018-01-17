b8f592754ad020b96fa84273850df4867f74ad15
PyroclasticMayhem <pyro@perhion.com>
2018-01-17 17:56:43 -0500

    Added Hugs

---
+++ commands/hugs.js
@@ -0,0 +1,21 @@
+/**
+ * Aru
+ * Hugs Command
+ */
+
+module.exports = function (bot, logger) {
+  bot.registerCommand('hug', (msg) => {
+    if (!msg.mentions.length) {
+      bot.createMessage(msg.channel.id, bot.user.username + ' gives ' + msg.author.mention + ' a soft hug')
+      logger.info(new Date() + `: Hug command used by ${msg.author.username}#${msg.author.discriminator} in ${msg.channel.guild.name} on self`) // Log command usage
+    } else {
+      var users = []
+      for (var i = 0; i < msg.mentions.length; i++) {
+        var user = msg.channel.guild.members.get(msg.mentions[i].id).id
+        users.push(`<@${user}>`)
+      }
+      bot.createMessage(msg.channel.id, msg.author.mention + ' hugged ' + users)
+      logger.info(new Date() + `: Hug command used by ${msg.author.username}#${msg.author.discriminator} in ${msg.channel.guild.name} on ${users}`) // Log command usage
+    }
+  })
+}
