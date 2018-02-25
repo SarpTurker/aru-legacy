c036ec42c8999ae34a85d9e506a3338bf9597d58
PyroclasticMayhem <pyro@perhion.com>
2018-02-25 13:04:55 -0500

    Remove Extra Space

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -9,7 +9,7 @@ module.exports = function (bot, guild, member, logger) {
   if (config.messages.join_leave[guild.id]) {
     bot.createMessage(config.messages.join_leave[guild.id].channel, config.messages.join_leave[guild.id].leave_message)
   }
-  
+
   // Log event
   logger.info(new Date() + `: ${member.username}#${member.discriminator} has left ${guild.name} ID#${guild.id}`)
 }
