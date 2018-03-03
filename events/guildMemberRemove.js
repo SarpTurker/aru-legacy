2c4d9445a5adeb547a9b3317ba694e9caec69ebf
PyroclasticMayhem <pyro@perhion.com>
2018-03-03 16:02:57 -0500

    Added New Parameters for Join/Leave Msg
    Username, discriminator, and id

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -10,7 +10,7 @@ module.exports = function (bot, guild, member, logger) {
     if (config.messages.join_leave[guild.id].channel_id && config.messages.join_leave[guild.id].leave_message) {
       guild.channels.find((channel) => {
         if (channel.id === config.messages.join_leave[guild.id].channel_id) {
-          bot.createMessage(config.messages.join_leave[guild.id].channel_id, config.messages.join_leave[guild.id].leave_message.replace('{mention}', member.mention).replace('{guild_name}', guild.name))
+          bot.createMessage(config.messages.join_leave[guild.id].channel_id, config.messages.join_leave[guild.id].leave_message.replace('{mention}', member.mention).replace('{guild_name}', guild.name).replace('{username}', member.username).replace('{discriminator}', member.discriminator).replace('{id}', member.id))
           logger.info(new Date() + `: ${member.username}#${member.discriminator} has left ${guild.name}`) // Log event
         }
       })
