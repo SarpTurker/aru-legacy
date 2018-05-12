063304e96b5195b6ce7f42ca0cf0f552cbea86bc
PyroclasticMayhem <pyro@perhion.com>
2018-05-12 11:59:36 -0400

    Change mongodb to use localhost and default Aru prefix

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 export API_PORT='5000'
-export MONGO_URL='mongodb://arudb:27017/aru'
-export PREFIX='!arut '
+export MONGO_URL='mongodb://localhost:27017/aru'
+export PREFIX='!aru '
 export TIME_FORMAT='ddd MMM DD YYYY | kk:mm:ss'
-export GAME_NAME='!arut help'
+export GAME_NAME='!aru help'
 export STREAM_URL=''
 export DISCORD_PW_KEY=''
 export CARBONITEX_KEY=''
